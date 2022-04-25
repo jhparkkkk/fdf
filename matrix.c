7ad91062a1923d1c9e87adb42a39bda23e6d3692
Jee hyun PARK <jeepark@e2r8p14.clusters.42paris.fr>
2022-04-25 18:39:08 +0200

    ok

--- matrix.c
+++ matrix.c
@@ -6,13 +6,34 @@
 /*   By: jeepark <jeepark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 10:11:18 by jeepark           #+#    #+#             */
-/*   Updated: 2022/04/24 14:44:13 by jeepark          ###   ########.fr       */
+/*   Updated: 2022/04/25 18:38:35 by jeepark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	iso(t_map *map, float *x, float *y, float z)
+// void    iso(t_point *coord, int lines, int map->col)
+void	iso(t_map *map, float *x, float *y, float *z)
+{
+    double    previous_x;
+    double    previous_y;
+
+	if (map->gap_z)
+		*z *= map->gap_z + map->zoom;
+    *x = round(*x - map->col / 2);
+    *y = round(*y - map->row / 2);
+    previous_x = *x;
+    previous_y = *y;
+    *x = round((previous_x - previous_y)
+            * cos(map->angle_x) * HEIGHT / round(map->col * 1.5 - map->zoom));
+    *y = round(-(*z * 5) + (previous_x + previous_y)
+            * sin(map->angle_x) * HEIGHT / round(map->col * 1.5 - map->zoom));
+    *z = round(*z);
+    *x = *x + WIDTH / 2;
+    *y = *y + HEIGHT / 2;
+}
+
+/*void	iso(t_map *map, float *x, float *y, float z)
 {
 	float	prior_x;
 	float	prior_y;
@@ -20,10 +41,10 @@ void	iso(t_map *map, float *x, float *y, float z)
 	if (map->gap_z)
 		z *= map->gap_z + map->zoom;
 	prior_x = *x - (map->col * scale_x(map) / 2);
-	prior_y = *y - (map->row * scale_y(map) / 2);
+	prior_y = *y - (map->row * scale_x(map) / 2);
 	*x = round((prior_x - prior_y) * cos(map->angle_x) + WIDTH / 2);
-	*y = round(-z + (prior_x + prior_y) * sin(M_PI / 6) + HEIGHT / 2);
-}
+	*y = round(-z + (prior_x + prior_y) * sin(map->angle_x) + HEIGHT / 2);
+}*/
 
 void	matrix_iso(t_map *map)
 {
@@ -36,7 +57,7 @@ void	matrix_iso(t_map *map)
 		j = 0;
 		while (j <= map->col)
 		{
-			iso(map, &map->trix[i][j].x, &map->trix[i][j].y, map->trix[i][j].z);
+			iso(map, &map->trix[i][j].x, &map->trix[i][j].y, &map->trix[i][j].z);
 			j++;
 		}
 		i++;
@@ -56,8 +77,8 @@ void	matrix_init(t_map *map)
 		j = 0;
 		while (j <= map->col)
 		{
-			map->trix[i][j].x = find_x(map, j);
-			map->trix[i][j].y = find_y(map, i);
+			map->trix[i][j].x = j;
+			map->trix[i][j].y = i;
 			map->trix[i][j].z = map->plan[i][j];
 			map->trix[i][j].color = ((map->trix[i][j].z) * 40 + (int)0xFF5733);
 			j++;
